97c72908159876163b0c64e83f28aae8c7d5d6aa
Comrak <jnardulli@hotmail.com>
2023-10-21 12:12:26 -0400

    se hace el merge de apiranking

--- js/apiranking.js
+++ js/apiranking.js
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-/* const url = 'https://myanimelist.p.rapidapi.com/manga/top/manga';
-const topMangas = document.querySelector("#topMangas")
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '77765b8a69msh0713a395a511ab9p139dacjsnd0a397940068',
-		'X-RapidAPI-Host': 'myanimelist.p.rapidapi.com'
-	}
-};
-
-async function fetchData() {
-    try {
-        const response = await fetch(url, options);
-        const result = await response.text();
-        console.log(result);
-    } catch (error) {
-        console.error(error);
-=======
 let data = [
     {
       "title": "Berserk",
@@ -567,15 +548,8 @@ let data = [
       "type": "Manga (6 vols)",
       "aired_on": "Feb 2014 - Jan 2018",
       "members": 54777
->>>>>>> 6e18f4d172d37ae0ee4fd6d945e928d93f759002
     }
 ]
-
-
-<<<<<<< HEAD
- */
-=======
-
 
 // const url = 'https://myanimelist.p.rapidapi.com/manga/top/all';
 // const topMangas = document.querySelector("#topMangas")
@@ -644,4 +618,3 @@ data.forEach(mangas => {
     topMangas.append(puntos);
     topMangas.append(rango);  
 })
->>>>>>> 6e18f4d172d37ae0ee4fd6d945e928d93f759002
